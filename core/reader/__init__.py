9895894817507a65c24688621d8a3db0b4e116d0
jamin <jamin@ja.min>
2019-12-10 17:49:30 +0900

    Init nested_ner

--- core/reader/__init__.py
+++ core/reader/__init__.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from .cg_reader import CGReader
 from .json_data_reader import JsonDataReader
 from .standoff_data_reader import StandoffDataReader
 from .tsv_data_reader import TSVDataReader
