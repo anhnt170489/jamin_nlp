041130b235eaf68796149ee4ac29b8deceb25911
jamin <jamin@ja.min>
2019-12-10 17:27:33 +0900

    Init GLUE

---
+++ core/models/bert_sequence_classification.py
@@ -0,0 +1,45 @@
+import numpy as np
+from torch import nn
+
+from core.common import *
+from libs import BertConfig, BertForSequenceClassification
+
+
+class BertSequenceClassification(nn.Module):
+    def __init__(
+            self, args
+    ) -> None:
+        super(BertSequenceClassification, self).__init__()
+        self._num_labels = len(args.labels)
+
+        config = BertConfig.from_pretrained(args.config_name if args.config_name else args.model_name_or_path,
+                                            num_labels=self._num_labels)
+        self._bert_for_sequence_classification = BertForSequenceClassification.from_pretrained(args.model_name_or_path,
+                                                                                               from_tf=bool(
+                                                                                                   '.ckpt' in args.model_name_or_path),
+                                                                                               config=config)
+
+    def forward(self, batch):
+        tokens = batch['tokens']
+        labels = batch['label'] if 'label' in batch else None
+
+        outputs = self._bert_for_sequence_classification(tokens[BERT_INPUT_IDS],
+                                                         attention_mask=tokens[BERT_INPUT_MASKS],
+                                                         token_type_ids=tokens[BERT_SEGMENT_IDS],
+                                                         labels=batch['label'])
+        if labels is not None:
+            loss, logits = outputs[:2]
+        else:
+            loss = None
+            logits = outputs[0]
+        if self._num_labels == 1:
+            #  We are doing regression
+            preds = np.squeeze(logits.detach().cpu().numpy())
+        else:
+            preds = logits.detach().cpu().numpy()
+            preds = np.argmax(preds, axis=1)
+
+        golds = labels.detach().cpu().numpy()
+
+        return {LOSS: loss, PREDICT: preds, GOLD: golds,
+                OUTPUT: outputs}
--- core/reader/__init__.py
+++ core/reader/__init__.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+from .glue_reader import ColaReader, MnliReader, MnliMismatchedReader, MrpcReader, QnliReader, QqpReader, RteReader, \
+    Sst2Reader, StsbReader, WnliReader
 from .json_data_reader import JsonDataReader
 from .standoff_data_reader import StandoffDataReader
 from .tsv_data_reader import TSVDataReader
