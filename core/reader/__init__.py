e6776161c417e11717e4e878c045a957dbb38140
jamin <jamin@ja.min>
2019-12-10 18:19:39 +0900

    update

--- core/reader/__init__.py
+++ core/reader/__init__.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-from .cg_reader import CGReader
 from .json_data_reader import JsonDataReader
 from .standoff_data_reader import StandoffDataReader
 from .tsv_data_reader import TSVDataReader
