ec351e79851fa207811a64531887e9c8bc97e239
jamin <jamin@ja.min>
2020-01-21 18:42:07 +0900

    Update due to changes in Transformers(libs)

--- libs/__init__.py
+++ libs/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-from .transformers.transformers.configuration_bert import BertConfig
-from .transformers.transformers.configuration_roberta import RobertaConfig
-from .transformers.transformers.modeling_bert import BertModel, BertForSequenceClassification, BertForQuestionAnswering
-from .transformers.transformers.optimization import AdamW, get_linear_schedule_with_warmup
-from .transformers.transformers.tokenization_bert import BertTokenizer
-from .transformers.transformers.tokenization_roberta import RobertaTokenizer
+from .transformers.src.transformers.configuration_bert import BertConfig
+from .transformers.src.transformers.configuration_roberta import RobertaConfig
+from .transformers.src.transformers.modeling_bert import BertModel, BertForSequenceClassification, BertForQuestionAnswering
+from .transformers.src.transformers.optimization import AdamW, get_linear_schedule_with_warmup
+from .transformers.src.transformers.tokenization_bert import BertTokenizer
+from .transformers.src.transformers.tokenization_roberta import RobertaTokenizer
