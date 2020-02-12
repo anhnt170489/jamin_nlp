d7ee327c2ddb5bc730645742c6e953b480e6fdb1
jamin <jamin@ja.min>
2020-02-12 16:33:53 +0900

    update

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,3 +1,4 @@
 # -*- coding: utf-8 -*-
+from .VCTransformers.configuration_vctrans import VCTransConfig
 from .bert_sequence_classification import BertSequenceClassification
 from .vctrans_sequence_classification import VCTransSequenceClassification
